0e8659e0e563624ca2fd52d4b25bb21d8f8a9d90
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-13 22:05:24 -0300

    mejoras en el template, bucle for ahora corre en el index.html

--- mvtBranda/FamiliaDB/views.py
+++ mvtBranda/FamiliaDB/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from FamiliaDB.models import *
-# Create your views here.
 
 def index(request):
     
-    parentezco = [x.parentezco for x in Familiar.objects.all()]
-    nombre = [x.nombre for x in Familiar.objects.all()]
-    edad = [x.edad for x in Familiar.objects.all()]
-    nacimiento = [x.nacimiento for x in Familiar.objects.all()]
+    familia = Familiar.objects.all()
+    mascotas = Mascota.objects.all()  
     
-    especie = [x.especie for x in Mascota.objects.all()]
-    nombre_mascota = [x.nombre for x in Mascota.objects.all()]
     
-    return render(request, 'FamiliaDB/index.html', {"parentezco": parentezco, "nombre": nombre, "edad": edad, "nacimiento": nacimiento, "especie":especie, "nombre_mascota":nombre_mascota})
+    ctx = {"familia": familia, "mascotas": mascotas} 
+    return render(request, 'FamiliaDB/index.html', ctx)
    
